0f1686b9766676c3b05fc79e939316bd78d66e9d
erinhmclark <erinclark@Erins-MacBook-Air.local>
2023-06-08 21:44:38 +0100

    Refactor srs_distribution_scraper.py, add docstrings.

--- srs_distribution_scraper.py
+++ srs_distribution_scraper.py
@@ -1,4 +1,4 @@
-""" Scrape branch and individual content from:
+""" Scrape branch and individual contact content from:
     https://www.srsdistribution.com/en/markets/find-a-branch/
 """
 import requests
@@ -12,6 +12,7 @@ SHEET_ID = ""
 
 
 def get_page_soup(url):
+    """ Return a BeautifulSoup object for a given url. """
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
     return soup
@@ -19,45 +20,53 @@ def get_page_soup(url):
 
 def get_data_list(soup):
     """ Get the main JSON data from JavaScript within a <script> tag. """
-
     script = soup.find('a', id="gdpr_optIn").findNext('script')
     script_str = script.string.split('active: undefined,')[-1].strip()
     data = chompjs.parse_js_object(script_str)
     return data
 
 
+def get_person_details(person):
+    """ Get the details of a single person from the branch details page.  """
+    names = person.find('h6').text.split()
+    first_name = ' '.join(names[0:-1])
+    last_name = names[-1]
+    job_role = person.find('span').text
+    email_address = person.find('a')['href'].replace('mailto:', '')
+    email_address = email_address if '@' in email_address else ''
+    phone_number_obj = person.find_all('a')[-1]
+    phone_number = phone_number_obj.text if 'tel' in phone_number_obj['href'] else ''
+
+    person_data = {
+        'first_name': first_name,
+        'last_name': last_name,
+        'job_role': job_role,
+        'email_address': email_address,
+        'phone_number': phone_number
+    }
+    return person_data
+
+
 def get_branch_details(branch):
+    """ Extract information for a branch. """
     branch_url = f'{BASE_URL}{branch["BranchUrl"]}'
     branch_soup = get_page_soup(branch_url)
     team_list = branch_soup.find('h4', text='Meet the Team').findNext('div') \
-                           .findAll('div', {'class': 'col-12 col-xs-6 col-sm-6 col-md-6 col-lg-6 col-xl-4 col-xxl-4 col-xxxl-3 mb-3'})
-
+                           .find_all('div',
+                           {'class': 'col-12 col-xs-6 col-sm-6 col-md-6 col-lg-6 col-xl-4 col-xxl-4 col-xxxl-3 mb-3'})
+    company = {'company_name': branch['BranchName']}
+    branch_data = {
+        'street_address': branch['StreetAddr'],
+        'city': branch['City'],
+        'state': branch['State'],
+        'zipcode': branch['ZipCd'],
+        'url': branch_url
+    }
     for person in team_list:
-        names = person.find('h6').text.split()
-        first_name = ' '.join(names[0:-1])
-        last_name = names[-1]
-        job_role = person.find('span').text
-        email_address = person.find('a')['href'].replace('mailto:', '')
-        if '@' not in email_address:
-            email_address = ''
-        phone_number = ''
-        if 'tel' in person.findAll('a')[-1]['href']:
-            phone_number = person.findAll('a')[-1].text
-
-        person_data = {
-            'company_name': branch['BranchName'],
-            'first_name': first_name,
-            'last_name': last_name,
-            'job_role': job_role,
-            'email_address': email_address,
-            'phone_number': phone_number,
-            'street_address': branch['StreetAddr'],
-            'city': branch['City'],
-            'state': branch['State'],
-            'zipcode': branch['ZipCd'],
-            'url': branch_url
-        }
-        insert_from_dict(SHEET_ID, 'Sheet1', person_data)
+        person_data = get_person_details(person)
+        # This ensures the correct order for the Google sheet:
+        final_dict = {**company, **person_data, **branch_data}
+        insert_from_dict(SHEET_ID, 'Sheet1', final_dict)
 
 
 if __name__ == '__main__':
