b305bc0a2578cda9ec8090b4cef64f7573422c49
erinhmclark <erinclark@Erins-MacBook-Air.local>
2023-06-12 19:03:48 +0100

    Move vars into separate settings.py file

---
+++ settings.py
@@ -0,0 +1,12 @@
+""" Settings file to store configurations or variables.
+    This file can be modified to read from the users config method of choice and as an intermediary.
+
+    ** Do not store passwords or secrets in this file **
+"""
+
+BASE_URL = "https://www.srsdistribution.com"
+SHEET_ID = ""
+SHEET_TAB = ""
+
+# This file should be created and completed as per the description in the README.
+SERVICE_ACCOUNT_JSON = "google_service_account.json"
